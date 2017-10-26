a374afae6cbc828f27607283fbb416933ef69340
alecgalba <alecgalba@gmail.com>
2017-10-26 16:21:45 -0400

    Terminated.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+let i = 0
 
-}
+function init () {
+
+  document.body.addEventListener('keydown', keyDown);
+
+  function keyDown (e){
+
+    const key = parseInt(e.detail || e.which || e.location);
+
+    if (key === code[i]) {
+      i++;
+
+      if (i === code.length) {
+        alert('Congratulations, world destruction mode activated...');
+        i = 0;
+      }
+    } else {
+      i = 0;
+    }
+  };
+};
